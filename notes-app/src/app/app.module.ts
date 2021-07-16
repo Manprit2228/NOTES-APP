7f0049456904b9998d48e984421ce76e9be4b093
Manprit <manpritkaur44209@gmail.com>
2021-07-16 20:03:33 +0530

    changes 7/16/2021

--- notes-app/src/app/app.module.ts
+++ notes-app/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { NotesListComponent } from './pages/notes-list/notes-list.component';
 import { MainLayoutComponent } from './pages/main-layout/main-layout.component';
+import { NoteCardComponent } from './note-card/note-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NotesListComponent,
-    MainLayoutComponent
+    MainLayoutComponent,
+    NoteCardComponent
   ],
   imports: [
     BrowserModule,
