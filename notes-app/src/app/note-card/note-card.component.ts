99fe9cbfc03fd531186f259b5b4e992d92e01bab
Manprit <manpritkaur44209@gmail.com>
2021-07-17 18:15:55 +0530

    "changes 7/17"

--- notes-app/src/app/note-card/note-card.component.ts
+++ notes-app/src/app/note-card/note-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Input, Renderer2 } from '@angular/core';
+import { Component, AfterViewInit ,ElementRef, OnInit, TestabilityRegistry, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-note-card',
@@ -7,9 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NoteCardComponent implements OnInit {
 
-  constructor() { }
+  @Input('title') title: string
+  
+  @Input('body') body: string
 
-  ngOnInit(): void {
+  // @Input() dataFromParent = 'QWERTY';
+  // todoData: any = [];
+
+  @ViewChild('truncator', {static: true, read: ElementRef} ) truncator: ElementRef<HTMLElement>;
+  @ViewChild('bodyText', {static: true, read: ElementRef} ) bodyText: ElementRef<HTMLElement>;
+
+  constructor(private renderer: Renderer2) { };
+
+  // ngAfterViewInit() 
+  ngOnInit()  {
+
+    //work out if there is a text overflow and if not, show the truncator    
+    let style = window.getComputedStyle(this.bodyText.nativeElement,null);
+    let viewableHeight = parseInt(style.getPropertyValue("height"),10);
+
+    if(this.bodyText.nativeElement.scrollHeight > viewableHeight) {
+      // if there is no text overflow, show the fade out truncator
+      this.renderer.setStyle(this.truncator.nativeElement, 'display', 'block');
+    }
+     //else: (there is a text overflow) hide the fade out truncator
+      else {
+        this.renderer.setStyle(this.truncator.nativeElement, 'display', 'none');
+      }
+    }
+
   }
 
-}
+
